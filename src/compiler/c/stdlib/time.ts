9ccb9a07aa34d5cfe5338f4fcddefed85fe86e97
Oleg Orlov <oleg.orlv@gmail.com>
2026-06-17 13:39:49 +0300

    Clean time runtime member path lookup

--- src/compiler/c/stdlib/time.ts
+++ src/compiler/c/stdlib/time.ts
@@ -1,10 +1,7 @@
+import { memberExpressionPath } from '../../member-paths.ts'
 import { timeRuntimeCFunctionNameFromPath } from '../../stdlib/descriptors/time.ts'
 import type { AnyNode } from '../../types.ts'
 
-export function cTimeRuntimeCallName(callee: AnyNode): string | null {
-  if (callee?.type !== 'MemberExpression' || callee.object.type !== 'Reference' || callee.object.path.length !== 1) {
-    return null
-  }
-
-  return timeRuntimeCFunctionNameFromPath([callee.object.path[0], callee.property])
+export function cTimeRuntimeCallName(callee: AnyNode | null | undefined): string | null {
+  return timeRuntimeCFunctionNameFromPath(memberExpressionPath(callee))
 }
