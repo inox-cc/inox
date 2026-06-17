685785bd7e1997f1c101654f49881e7c5962dd48
Oleg Orlov <oleg.orlv@gmail.com>
2026-06-17 13:31:58 +0300

    Clean runtime builtin source set construction

--- src/compiler/runtime-builtins.ts
+++ src/compiler/runtime-builtins.ts
@@ -1,20 +1,33 @@
-import {
-  implementedRuntimeBuiltinImportSources,
-  unsupportedRuntimeBuiltinImportSources
-} from './stdlib/descriptors/node-builtins.ts'
-
 export const runtimeBuiltinImportSources = createRuntimeBuiltinImportSources()
 
 function createRuntimeBuiltinImportSources(): Set<string> {
   const sources: Set<string> = new Set()
 
-  for (const specifier of implementedRuntimeBuiltinImportSources) {
-    sources.add(specifier)
-  }
-
-  for (const specifier of unsupportedRuntimeBuiltinImportSources) {
-    sources.add(specifier)
-  }
+  sources.add('dgram')
+  sources.add('fs')
+  sources.add('http')
+  sources.add('net')
+  sources.add('node:path')
+  sources.add('node:buffer')
+  sources.add('node:crypto')
+  sources.add('node:child_process')
+  sources.add('node:dgram')
+  sources.add('node:events')
+  sources.add('node:fs')
+  sources.add('node:fs/promises')
+  sources.add('node:http')
+  sources.add('node:net')
+  sources.add('node:os')
+  sources.add('node:stream')
+  sources.add('node:timers')
+  sources.add('node:url')
+  sources.add('node:process')
+  sources.add('node:dns')
+  sources.add('node:https')
+  sources.add('node:timers/promises')
+  sources.add('node:tls')
+  sources.add('node:worker_threads')
+  sources.add('node:zlib')
 
   return sources
 }
