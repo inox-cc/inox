1f2570927102aa02e5fa61b175b9878f2aa24492
Oleg Orlov <oleg.orlv@gmail.com>
2026-06-29 01:08:17 +0300

    Document C++ class lowering and trim simple example

--- examples/simple/src/index.ts
+++ examples/simple/src/index.ts
@@ -1,9 +1,9 @@
 console.log('hello world')
-console.log('date', Date.now())
-console.log('performance', performance.now())
-console.log('process.version', process.version)
-console.log('process.versions', process.versions)
-console.log('process', process)
+// console.log('date', Date.now())
+// console.log('performance', performance.now())
+// console.log('process.version', process.version)
+// console.log('process.versions', process.versions)
+// console.log('process', process)
 
 // try {
 //   const res = await fetch('https://example.com/')
@@ -14,31 +14,46 @@ console.log('process', process)
 //   console.log('#error:', error)
 // }
 
-try {
-  const foo = JSON.parse('{"v":{"1":2},{"3":4,"5":6}]}') // bad json
-  console.log(foo)
-} catch (e) {
-  console.error('Error', e)
+// try {
+//   const foo = JSON.parse('{"v":{"1":2},{"3":4,"5":6}]}') // bad json
+//   console.log(foo)
+// } catch (e) {
+//   console.error('Error', e)
+// }
+//
+// const foo = JSON.parse('{"v":[{"1":2},{"3":4,"5":6}]}')
+//
+// console.log(Array.isArray(foo))
+//
+// console.log(Object.entries(foo.v))
+//
+// for (const a of foo.v) {
+//   console.log(a)
+//
+//   const b = Object.values(a)
+//   console.log(b)
+//
+//   const c = Object.values(a)[0]
+//   console.log(c)
+//
+//   const d = Object.entries(a)
+//   console.log(d)
+//
+//   const e = Object.entries(a)[0]
+//   console.log(e)
+// }
+
+class Foo {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+
+  test() {
+    console.log(this.name)
+  }
 }
 
-const foo = JSON.parse('{"v":[{"1":2},{"3":4,"5":6}]}')
-
-console.log(Array.isArray(foo))
-
-console.log(Object.entries(foo.v))
-
-for (const a of foo.v) {
-  console.log(a)
-
-  const b = Object.values(a)
-  console.log(b)
-
-  const c = Object.values(a)[0]
-  console.log(c)
-
-  const d = Object.entries(a)
-  console.log(d)
-
-  const e = Object.entries(a)[0]
-  console.log(e)
-}
+const f = new Foo('foo 1')
+f.test()
