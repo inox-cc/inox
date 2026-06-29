a196aadb7ac67a4cf2dd5071292a54d226df8f69
Oleg Orlov <oleg.orlv@gmail.com>
2026-06-29 14:38:56 +0300

    Cover nested class descriptor object APIs

---
+++ tests/features/cases/class-object-entries-nested-instance-field.test.ts
@@ -0,0 +1,23 @@
+// @targets cc
+// @expect pass
+// @stdout [[parent, Parent { name: Ada }]]
+
+class Parent {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+class Child {
+  parent: Parent
+
+  constructor(parent: Parent) {
+    this.parent = parent
+  }
+}
+
+const parent = new Parent('Ada')
+const child = new Child(parent)
+console.log(Object.entries(child))
