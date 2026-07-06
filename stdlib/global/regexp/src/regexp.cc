80e13df49e9550bf6452fceff1402c7791b7b29e
Oleg Orlov <oleg.orlv@gmail.com>
2026-07-06 13:31:32 +0300

    Inline regexp test implementation

--- stdlib/global/regexp/src/regexp.cc
+++ stdlib/global/regexp/src/regexp.cc
@@ -32,15 +32,21 @@ static char* regexp_copy_null_terminated(inox::StringView value) {
   return bytes;
 }
 
-static bool regexp_test(inox::StringView pattern, RegExpFlags flags, inox::StringView value) {
-  char* pattern_bytes = regexp_copy_null_terminated(pattern);
+RegExp::RegExp() : pattern_(""), flags_(RegExpFlags::None) {}
+
+RegExp::RegExp(inox::StringView pattern) : pattern_(pattern), flags_(RegExpFlags::None) {}
+
+RegExp::RegExp(inox::StringView pattern, RegExpFlags flags) : pattern_(pattern), flags_(flags) {}
+
+bool RegExp::test(inox::StringView value) const {
+  char* pattern_bytes = regexp_copy_null_terminated(pattern_);
 
   if (pattern_bytes == nullptr) {
     return false;
   }
 
   regex_t regex;
-  int status = regcomp(&regex, pattern_bytes, REG_EXTENDED | regexp_native_flags(flags));
+  int status = regcomp(&regex, pattern_bytes, REG_EXTENDED | regexp_native_flags(flags_));
   free(pattern_bytes);
 
   if (status != 0) {
@@ -61,17 +67,33 @@ static bool regexp_test(inox::StringView pattern, RegExpFlags flags, inox::Strin
   return status == 0;
 }
 
-RegExp::RegExp() : pattern_(""), flags_(RegExpFlags::None) {}
-
-RegExp::RegExp(inox::StringView pattern) : pattern_(pattern), flags_(RegExpFlags::None) {}
-
-RegExp::RegExp(inox::StringView pattern, RegExpFlags flags) : pattern_(pattern), flags_(flags) {}
-
-bool RegExp::test(inox::StringView value) const {
-  return regexp_test(pattern_, flags_, value);
-}
-
 bool RegExp::test(const inox::Value& value) const {
   const inox::String string(value);
-  return regexp_test(pattern_, flags_, inox::StringView(string.bytes(), string.length()));
+  const inox::StringView view(string.bytes(), string.length());
+  char* pattern_bytes = regexp_copy_null_terminated(pattern_);
+
+  if (pattern_bytes == nullptr) {
+    return false;
+  }
+
+  regex_t regex;
+  int status = regcomp(&regex, pattern_bytes, REG_EXTENDED | regexp_native_flags(flags_));
+  free(pattern_bytes);
+
+  if (status != 0) {
+    return false;
+  }
+
+  char* value_bytes = regexp_copy_null_terminated(view);
+
+  if (value_bytes == nullptr) {
+    regfree(&regex);
+    return false;
+  }
+
+  status = regexec(&regex, value_bytes, 0, 0, 0);
+  free(value_bytes);
+  regfree(&regex);
+
+  return status == 0;
 }
