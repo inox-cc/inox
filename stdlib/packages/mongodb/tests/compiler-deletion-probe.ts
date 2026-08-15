04ef5671d0f893cb29da976b1581b910ca3fa6db
Oleg Orlov <oleg.orlv@gmail.com>
2026-08-15 17:28:22 +0300

    Move MongoDB package out of Inox

--- stdlib/packages/mongodb/tests/compiler-deletion-probe.ts
+++
@@ -1,5 +0,0 @@
-export const compilerLibraryDeletionProbe = {
-  source: "import { ObjectId } from 'mongodb'\nnew ObjectId()\n",
-  presentDiagnosticCodes: ['INOX_MONGODB_LOOP_BACKEND'],
-  absentDiagnosticCodes: ['INOX_UNSUPPORTED_IMPORT_SOURCE', 'INOX_UNKNOWN_NAME']
-}
